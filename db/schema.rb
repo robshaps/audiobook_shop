57fd15491814852959b522e46adccee37223629d
Rob Shapiro <robshaps78@gmail.com>
2013-03-10 15:18:55 -0400

    Testing a commit.. did not really change anything

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130310183149) do
+ActiveRecord::Schema.define(:version => 20130310184232) do
 
   create_table "carts", :force => true do |t|
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -27,6 +28,8 @@ ActiveRecord::Schema.define(:version => 20130310183149) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
