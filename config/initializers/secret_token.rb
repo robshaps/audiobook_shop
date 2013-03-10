04c3c379db87f850cf4b54b88342eca602293122
Rob Shapiro <robshaps78@gmail.com>
2013-03-10 12:57:34 -0400

    Created a rails app for my audiobook shop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AudiobookShop::Application.config.secret_token = '4f1c1020ea6032e1d1923ca4d8bb25bacf8e49ee8aec2a49874aa236c59556bbba94e5ac8ffb4351f3c72423e262633489df8aed6a4cae5197f1036b6cbdff92'
